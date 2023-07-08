506d4a61cc44ba4f8629a0c6a6ad5e5466d6a9e3
ajeet <ajeettiwari.ajju21may@gmail.com>
2023-07-08 18:09:32 +0530

    database replaced with atlas

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ import mongoose from "mongoose";
 import router from "./routes/userroutes.js";
 import blogRouter from "./routes/blog-routes.js"; 
 import cors from "cors";
+import * as dotenv from 'dotenv'
+dotenv.config();
 const app=express();    
 app.use(cors());
 app.use(express.json());  
 app.use("/api/user",router) ; 
 app.use("/api/blog",blogRouter); 
 
-mongoose.connect("mongodb://localhost:27017",{
-    dbName:"Blog"
-}).then(()=>app.listen(5000)) 
+mongoose.connect(process.env.CONNECTION_URL).then(()=>app.listen(5000)) 
   .then(()=>console.log("connected to database"))
   .catch((err)=>console.log(err))
 
